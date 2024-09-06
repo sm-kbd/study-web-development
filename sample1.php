d5fe7eeccaafe1a33a86044a7aed2efd66c40743
sm-kbd <kd1404385@st.kobedenshi.ac.jp>
2024-09-06 10:46:13 +0900

    First commit.

---
+++ sample1.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+
+<html lang="ja">
+
+  <head>
+
+    <meta charset="UTF-8" />
+
+    <meta name="title" content="web開発4回目" />
+
+  </head>
+
+  <body>
+
+ スシュミト
+
+  </body>
+
+</html>
